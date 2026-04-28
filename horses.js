da8a1912927035d69a754a34a9d1c2732e74e5b8
SavvierThestral <nico.ferrey.i@gmail.com>
2026-04-28 17:58:02 -0300

    Remove Vimeo video embed from Timeno page

--- horses.js
+++ horses.js
@@ -69,7 +69,6 @@ const horses = [
 
         capa: "Castaña",
         piro: "Negativo",
-        vimeo: "1187374122",
         image: "caballos/TIMEÑO/Timeno2-800.webp",
         images: ["caballos/TIMEÑO/Timeno2-800.webp", "caballos/TIMEÑO/timeño.webp"],
 
